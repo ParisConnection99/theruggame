c2f4f3fddb2ae16b537e143aee35c4f3e841ae74
Bread Head <breadhead101@proton.me>
2025-03-14 03:00:31 +0000

    remove open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,20 +23,21 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "The Rug Game",
   description: "Guess pump or rug correctly & win Big!",
-  openGraph: {
-    title: "The Rug Game",
-    description: "Guess pump or rug correctly & win Big!",
-    url: "https://theruggame.fun/",
-    siteName: "The Rug Game",
-    images: [
-      {
-        url: "https://theruggame.fun/images/logo1.png",
-        width: 1200,
-        height: 630,
-      }
-    ],
-    type: "website",
-  },
+  // openGraph: {
+  //   title: "The Rug Game",
+  //   description: "Guess pump or rug correctly & win Big!",
+  //   url: "https://theruggame.fun/",
+  //   siteName: "The Rug Game",
+  //   images: [
+  //     {
+  //       url: "https://theruggame.fun/images/logo1.png",
+  //       width: 1200,
+  //       height: 630,
+  //     }
+  //   ],
+  //   locale: 'en_US',
+  //   type: "website",
+  // },
   twitter: {
     card: "summary_large_image",
     title: "The Rug Game",
@@ -51,12 +52,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <head>
-        <meta property="og:title" content="The Rug Game" />
-        <meta property="og:image" content="https://theruggame.fun/images/logo1.png" />
-        <meta property="og:description" content="Guess pump or rug correctly & win Big!" />
-        <meta property="og:url" content="https://theruggame.fun/" />
-      </head> */}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-900`}>
         <GlobalErrorHandler>
           <ClientProviders>
