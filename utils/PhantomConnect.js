8da15552c657624c2a0240f0a263dfef702a874c
Bread Head <breadhead101@proton.me>
2025-03-20 10:36:57 +0000

    updated phantom connect

--- utils/PhantomConnect.js
+++ utils/PhantomConnect.js
@@ -36,20 +36,54 @@ const buildUrl = (path, params) =>
 
 class PhantomConnect {
     constructor() {
+        // Check if we're in a browser environment
+        if (typeof window === 'undefined') {
+            return;
+        }
+
         // Initialize with stored keypair or create new one
-        const storedPrivateKey = localStorage.getItem('dappEncryptionPrivateKey');
-        const storedPublicKey = localStorage.getItem('dappEncryptionPublicKey');
+        const storedPrivateKey = window.localStorage.getItem('dappEncryptionPrivateKey');
+        const storedPublicKey = window.localStorage.getItem('dappEncryptionPublicKey');
 
         if (storedPrivateKey && storedPublicKey) {
-            this.dappKeyPair = {
-                publicKey: bs58.decode(storedPublicKey),
-                secretKey: bs58.decode(storedPrivateKey)
-            };
+            try {
+                // Use existing keypair
+                const existingKeypair = nacl.box.keyPair.fromSecretKey(bs58.decode(storedPrivateKey));
+                this.dappKeyPair = existingKeypair;
+                
+                logInfo('Using existing keypair', {
+                    component: 'PhantomConnect',
+                    publicKey: storedPublicKey
+                });
+            } catch (error) {
+                logError(error, {
+                    component: 'PhantomConnect',
+                    action: 'loading existing keypair'
+                });
+                // If there's an error with stored keys, remove them and generate new ones
+                window.localStorage.removeItem('dappEncryptionPrivateKey');
+                window.localStorage.removeItem('dappEncryptionPublicKey');
+                this.generateNewKeypair();
+            }
         } else {
-            this.dappKeyPair = nacl.box.keyPair();
-            localStorage.setItem('dappEncryptionPublicKey', bs58.encode(this.dappKeyPair.publicKey));
-            localStorage.setItem('dappEncryptionPrivateKey', bs58.encode(this.dappKeyPair.secretKey));
+            // No existing keypair, generate new one
+            this.generateNewKeypair();
         }
+    }
+
+    generateNewKeypair() {
+        if (typeof window === 'undefined') {
+            return;
+        }
+
+        this.dappKeyPair = nacl.box.keyPair();
+        window.localStorage.setItem('dappEncryptionPublicKey', bs58.encode(this.dappKeyPair.publicKey));
+        window.localStorage.setItem('dappEncryptionPrivateKey', bs58.encode(this.dappKeyPair.secretKey));
+        
+        logInfo('Generated new keypair', {
+            component: 'PhantomConnect',
+            publicKey: bs58.encode(this.dappKeyPair.publicKey)
+        });
     }
 
     connect() {
@@ -106,5 +140,7 @@ class PhantomConnect {
     }
 }
 
-export const phantomConnect = new PhantomConnect();
+// Only create instance if in browser environment
+const phantomConnect = typeof window !== 'undefined' ? new PhantomConnect() : null;
+
 export { decryptPayload, encryptPayload, buildUrl }; 
