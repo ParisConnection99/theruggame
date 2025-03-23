a6d300ece138347f2db64e9f10bf6c8874221f5d
Bread Head <breadhead101@proton.me>
2025-03-23 15:45:43 +0000

    updated firebase admin code in route

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -2,20 +2,20 @@ import admin from 'firebase-admin';
 
 // Initialize admin if not already initialized
 if (!admin.apps.length) {
-    try {
-      const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-      admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: privateKey,
-        })
-      });
-      console.log("Firebase Admin initialized successfully");
-    } catch (error) {
-      console.error("Firebase Admin initialization error:", error);
-    }
+  try {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'); // Replace escaped newlines
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: privateKey,
+      }),
+    });
+    console.log("Firebase Admin initialized successfully");
+  } catch (error) {
+    console.error("Firebase Admin initialization error:", error);
   }
+}
 
   export async function POST(request) {
     try {
