a89e15bc3f2184b69a7c80e7c501ffa0a740d699
Bread Head <breadhead101@proton.me>
2025-04-08 18:35:44 +0100

    added useEffect to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
-import React from "react";
+import React, { useEffect } from "react";
 import ClientProviders from '@/components/ClientProviders';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
